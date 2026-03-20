942394502b9bf0bb3625922fab5c1c6431375df3
Libório Júnior <libajunior@gmail.com>
2026-03-20 10:31:29 -0300

    Aula 08 - 19/03

---
+++ Aula08/src/main/java/br/com/unimater/aed2026/exemplos/Exemplo09.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.unimater.aed2026.exemplos;
+
+/**
+ *
+ * @author libajunior
+ */
+public class Exemplo09 {
+    public static void main(String[] args) {
+        
+        //Contar até 10 de um em um
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Número "+ i);
+        }
+        
+        //Contar pares
+        for (int i = 0; i < 10; i += 2 ) {
+            System.out.println("Pares: "+ i);
+        }
+    }
+}
---
+++ Aula08/src/main/java/br/com/unimater/aed2026/exemplos/Exemplo10.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.unimater.aed2026.exemplos;
+
+/**
+ *
+ * @author libajunior
+ */
+public class Exemplo10 {
+    public static void main(String[] args) {
+        
+        //Contar regressiva
+        for (int i = 10; i >= 0; i--) {
+            System.out.println("Número "+ i);
+        }
+    }
+}
